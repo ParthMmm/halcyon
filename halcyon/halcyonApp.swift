af302751ba2276ddedff678bdef8584a7eef8980
Parth Mangrola <parthmangrola@gmail.com>
2025-10-01 17:00:36 -0700

    Initial Commit

---
+++ halcyon/halcyonApp.swift
@@ -0,0 +1,17 @@
+//
+//  halcyonApp.swift
+//  halcyon
+//
+//  Created by Parth Mangrola on 10/1/25.
+//
+
+import SwiftUI
+
+@main
+struct halcyonApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ halcyonTests/halcyonTests.swift
@@ -0,0 +1,17 @@
+//
+//  halcyonTests.swift
+//  halcyonTests
+//
+//  Created by Parth Mangrola on 10/1/25.
+//
+
+import Testing
+@testable import halcyon
+
+struct halcyonTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
